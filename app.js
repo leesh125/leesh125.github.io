1965fd09b716fac6ce241d29f6c2e5c3d4da30ad
leesh125 <chelsea12345@naver.com>
2021-12-15 02:45:31 +0900

    Loading Username

--- app.js
+++ app.js
@@ -4,24 +4,26 @@ const greeting = document.querySelector("#greeting");
 const link = document.querySelector("a");
 
 const HIDDEN_CLASSNAME = "hidden";
+const USERNAME_KEY = "username";
 
 function onLoginSubmit(event) { // submit 이벤트가 발생할 때 필요한 정보들을 담은 object
     event.preventDefault(); // 기본 동작을 실행하지 못하게 막기
     loginForm.classList.add(HIDDEN_CLASSNAME);
     const username = loginInput.value;
-    localStorage.setItem("username", username);
+    localStorage.setItem(USERNAME_KEY, username);
+    paintGreetings(username);
+}
+
+function paintGreetings(username) {
     greeting.innerText = `Hello ${username}`; 
     greeting.classList.remove(HIDDEN_CLASSNAME);
 }
 
-loginForm.addEventListener("submit", onLoginSubmit); // 브라우저는 함수 호출 시 기본적으로 인자를 담아서 호출한다.
+const savedUsername = localStorage.getItem(USERNAME_KEY);
 
-
-
-// 링크를 타고 넘어가는걸 막아줌
-// function handleLinkClick(event) { 
-//     event.preventDefault();
-//     console.log(event);
-// }
-// link.addEventListener("click", handleLinkClick);
-
+if (savedUsername === null){
+    loginForm.classList.remove(HIDDEN_CLASSNAME);
+    loginForm.addEventListener("submit", onLoginSubmit);     
+} else {
+    paintGreetings(savedUsername);
+}
