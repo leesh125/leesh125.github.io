d771643259499b83c0ada3a9ceea194acba9725d
leesh125 <chelsea12345@naver.com>
2021-12-15 02:11:06 +0900

    Events Object(PreventDefault-Refresh_F5)

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
+const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
-const loginButton = document.querySelector("#login-form button");
+const greeting = document.querySelector("#greeting");
+const link = document.querySelector("a");
 
-function onLoginBtnClick() {
-console.dir(loginInput);
-}
+const HIDDEN_CLASSNAME = "hidden";
 
-loginButton.addEventListener("click",onLoginBtnClick);
+function onLoginSubmit(event){
+    event.preventDefault(); // 브라우저가 기본 동작을 실행하지 못하게 막기 // event object는 preventDefault함수를 기본적으로 갖고 있음
+    console.log(event);
+    }
+    
+    loginForm.addEventListener("submit", onLoginSubmit); // submit 이벤트가 발생한다면, onLoginSubmit함수를 실행시킨다는 의미 // JS는 onLoginSubmit함수 호출시 인자를 담아서 호출함. 해당 인자는 event object를 담은 정보들
