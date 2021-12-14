b1fa40985ddb49227abf7b564b91931ca57b9fff
leesh125 <chelsea12345@naver.com>
2021-12-15 02:12:43 +0900

    Events2 (Prevent link event)

--- app.js
+++ app.js
@@ -5,9 +5,12 @@ const link = document.querySelector("a");
 
 const HIDDEN_CLASSNAME = "hidden";
 
-function onLoginSubmit(event){
-    event.preventDefault(); // 브라우저가 기본 동작을 실행하지 못하게 막기 // event object는 preventDefault함수를 기본적으로 갖고 있음
+
+
+//링크를 타고 넘어가는걸 막아줌
+function handleLinkClick(event) { 
+    event.preventDefault();
     console.log(event);
-    }
-    
-    loginForm.addEventListener("submit", onLoginSubmit); // submit 이벤트가 발생한다면, onLoginSubmit함수를 실행시킨다는 의미 // JS는 onLoginSubmit함수 호출시 인자를 담아서 호출함. 해당 인자는 event object를 담은 정보들
+}
+link.addEventListener("click", handleLinkClick);
+
