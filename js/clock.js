6ce372a7d33108315be5914e6337b5c76ecbc8e9
leesh125 <chelsea12345@naver.com>
2021-12-15 18:15:00 +0900

    Timeouts and Dates obj

--- js/clock.js
+++ js/clock.js
@@ -1,8 +1,48 @@
 const clock = document.querySelector("h2#clock");
 
-function sayHello() {
-    console.log("hello");
+function getClock() {
+    const date = new Date();
+
+    clock.innerText = `${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`;
 }
 
-// setInterval(함수명, ms) - 특정시간마다 반복적으로 실행이 된다.
-setInterval(sayHello, 5000);
+// setTimeout - 인자로 준 ms 만큼 기다렸다가 한번만 실행
+getClock();
+setInterval(getClock, 1000);
+
+
+
+
+
+// new Date()
+// Wed Dec 15 2021 18:06:18 GMT+0900 (한국 표준시)
+// const date = new Date();
+// undefined
+// date.getDate()
+// 15
+// date.getDay()
+// 3
+// date.getYear()
+// 121
+// date.getFullYear()
+// 2021
+// date.getHours()
+// 18
+// date.getMinutes()
+// 6
+// date.getSeconds()
+// 41
+// new Date().getHours()
+// 18
+// new Date().getMinutes()
+// 8
+// new Date().getSeconds()
+// 26
+// new Date().getSeconds()
+// 26
+// new Date().getSeconds()
+// 27
+// new Date().getSeconds()
+// 29
+// new Date().getSeconds()
+// 30
