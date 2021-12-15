e0646648e42a5f035b8b28863bf9bf2053eadea2
leesh125 <chelsea12345@naver.com>
2021-12-15 18:29:08 +0900

    padStart, padEnd

--- js/clock.js
+++ js/clock.js
@@ -2,8 +2,11 @@ const clock = document.querySelector("h2#clock");
 
 function getClock() {
     const date = new Date();
-
-    clock.innerText = `${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`;
+    const hours = String(date.getHours()).padStart(2,"0");
+    const minutes = String(date.getMinutes()).padStart(2,"0");
+    const second = String(date.getSeconds()).padStart(2,"0");
+    clock.innerText = `${hours}:${minutes}:${second}`;
+    
 }
 
 // setTimeout - 인자로 준 ms 만큼 기다렸다가 한번만 실행
@@ -11,38 +14,12 @@ getClock();
 setInterval(getClock, 1000);
 
 
-
-
-
-// new Date()
-// Wed Dec 15 2021 18:06:18 GMT+0900 (한국 표준시)
-// const date = new Date();
-// undefined
-// date.getDate()
-// 15
-// date.getDay()
-// 3
-// date.getYear()
-// 121
-// date.getFullYear()
-// 2021
-// date.getHours()
-// 18
-// date.getMinutes()
-// 6
-// date.getSeconds()
-// 41
-// new Date().getHours()
-// 18
-// new Date().getMinutes()
-// 8
-// new Date().getSeconds()
-// 26
-// new Date().getSeconds()
-// 26
-// new Date().getSeconds()
-// 27
-// new Date().getSeconds()
-// 29
-// new Date().getSeconds()
-// 30
+// ## padstart , end
+// "1".padStart(2,"0");
+// '01'
+// "12".padStart(2, "0");
+// '12'
+// "1".padEnd(2,"0");
+// '10'
+// "hello".padStart(20, "x");
+// 'xxxxxxxxxxxxxxxhello'
