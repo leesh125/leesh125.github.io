09fe808c074831487dfaf8627f515ee70b2ad4ea
leesh125 <chelsea12345@naver.com>
2021-12-18 18:03:34 +0900

    Loading todos & Doing something with localstorage var

--- js/todo.js
+++ js/todo.js
@@ -2,13 +2,15 @@ const todoForm = document.getElementById("todo-form");
 const todoInput = todoForm.querySelector("input");
 const todoList = document.getElementById("todo-list");
 
+const TODOS_KEY = "todos";
+
 const toDos = [];
 
 function saveToDos() {
     console.log(toDos);
      // toDos (리스트)객체를 문자열로 변환시킨다.
      // 이유 : localstorage 에서 값을 불러오기 위해
-    localStorage.setItem("todos", JSON.stringify(toDos));
+    localStorage.setItem(TODOS_KEY, JSON.stringify(toDos));
 }
 
 function deleteToDo(event) {
@@ -39,10 +41,20 @@ function handleToDoSubmit(event) {
 
 todoForm.addEventListener("submit", handleToDoSubmit);
 
-const player = {
-    name: "asd",
+// JSON.stringify()로 배열형태의 문자열인 toDos를 가지고온다 ( "["a","b"]" ")
+const savedToDos = localStorage.getItem(TODOS_KEY);
+
+function say(item) {
+    console.log("say" ,item);
 }
 
-// 자바스크립트 object, array 뿐만 아니라 자바스크립트 코드 등 어떤것이든간에
-// string으로 만들어준다.
-JSON.stringify(player);   
+if(saveToDos) {
+    // 배열형태의 문자열을 parse() 를 통해 배열로 만들어준다
+    const parsedToDos = JSON.parse(savedToDos);
+    // parseToDos 리스트가 가지고 있는 원소에 say 함수를 실행
+    // 원소가 3개있으면 3번 실행 
+    // 밑에 두개는 동일한 코드 (1번 함수 씀, 2번 함수 안씀(화살표 함수 씀))
+    parsedToDos.forEach(say);
+    parsedToDos.forEach((item) => console.log("say", item));
+    
+}
