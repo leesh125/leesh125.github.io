31a4f450592b79d34a8562e3d1a91dad271b5aab
leesh125 <chelsea12345@naver.com>
2021-12-18 18:47:33 +0900

    Adopt filter func to todoList, to remove what i want to delete

--- js/todo.js
+++ js/todo.js
@@ -55,3 +55,23 @@ if(savedToDos !== null) {
     toDos = parsedToDos;
     parsedToDos.forEach(paintToDo);
 }
+
+function sexyFilter(item) {
+    // return true; // 1,2,3,4가 그대로 보존
+    // return false; // 배열에서 1,2,3,4 제외 => 빈배열이 됨
+
+    // item이 3이 아니면 true리턴 => 3은 배열에서 제외 ([1,2,4])
+    return item !== 3;
+}
+
+// filter가 함수를 4번부름(인자가 바뀜(1,2,3,4 순서))
+// filter함수에서 sexyfilter 함수를 실행할때 해당 인자가 true면 그대로 배열에 남고 false면 배열에서 제외
+[1,2,3,4].filter(sexyFilter) 
+
+function aafilter(num) { return num > 2 }
+arr.filter(aafilter) // [3]
+
+
+const todos = [{"text":"asd","id":1639819772729},{"text":"qwe","id":16398}];
+function sexyfilter(todo) { return todo.id !== 1639819772729 }
+todos.filter(sexyfilter) // {text: 'qwe', id: 16398}
