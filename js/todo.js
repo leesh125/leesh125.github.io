0627cbdc0648ecce7a8b28f68d2fe374c5e7b4b5
leesh125 <chelsea12345@naver.com>
2021-12-18 17:44:49 +0900

    Store todoList in localstorage

--- js/todo.js
+++ js/todo.js
@@ -2,6 +2,15 @@ const todoForm = document.getElementById("todo-form");
 const todoInput = todoForm.querySelector("input");
 const todoList = document.getElementById("todo-list");
 
+const toDos = [];
+
+function saveToDos() {
+    console.log(toDos);
+     // toDos (리스트)객체를 문자열로 변환시킨다.
+     // 이유 : localstorage 에서 값을 불러오기 위해
+    localStorage.setItem("todos", JSON.stringify(toDos));
+}
+
 function deleteToDo(event) {
     const li = event.target.parentElement;
     li.remove();
@@ -23,7 +32,17 @@ function handleToDoSubmit(event) {
     event.preventDefault(); 
     const newTodo = todoInput.value;  // 입력한 문자열을 저장
     todoInput.value = ""; 
+    toDos.push(newTodo);
     paintToDo(newTodo);
+    saveToDos();
 }
 
 todoForm.addEventListener("submit", handleToDoSubmit);
+
+const player = {
+    name: "asd",
+}
+
+// 자바스크립트 object, array 뿐만 아니라 자바스크립트 코드 등 어떤것이든간에
+// string으로 만들어준다.
+JSON.stringify(player);   
